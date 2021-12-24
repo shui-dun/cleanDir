ec4cf0429c2beebfa7c9c0fd2c5da474ddf80a2a
shui-dun <2604697844@qq.com>
2021-12-24 19:32:58 +0800

    清理淘宝保存的文件

--- app/src/main/java/com/sd/cleardir/MainActivity.java
+++ app/src/main/java/com/sd/cleardir/MainActivity.java
@@ -26,12 +26,20 @@ public class MainActivity extends AppCompatActivity {
 
     // 点击开始清理
     public void onClick(View v) {
+        // 很多软件把保存的图片放在这里
         clearDirWithoutDirs("/storage/emulated/0/Pictures");
+        // telegram的图片以及文件
         clearDirWithoutDirs("/storage/emulated/0/Telegram");
+        // 腾讯的文件基本上看到私有目录下的，但也有一点放在这里
         clearDirWithoutDirs("/storage/emulated/0/Tencent");
+        // 截图
         clearDir("/storage/emulated/0/DCIM/Screenshots");
+        // 截屏
         clearDir("/storage/emulated/0/DCIM/ScreenRecorder");
+        // 下载目录
         clearDir("/storage/emulated/0/Download");
+        // 有一些app将保存的图片直接放在DCIM下，比如淘宝
+        clearShallowDir("/storage/emulated/0/DCIM");
         Toast.makeText(getApplicationContext(), "清理结束", Toast.LENGTH_SHORT).show();
     }
 
@@ -112,5 +120,19 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "删除目录：" + filePath + "失败！", Toast.LENGTH_SHORT).show();
             return false;
         }
+    }
+
+    // 只删除一级目录下的文件
+    private boolean clearShallowDir(String filePath) {
+        File dirFile = new File(filePath);
+        File[] files = dirFile.listFiles();
+        for (File file : files) {
+            if (file.isFile()) {
+                if (!deleteSingleFile(file.getAbsolutePath())) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
